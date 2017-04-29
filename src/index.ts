175209c3d939062fac8ecade97444ae41331305b
Martin Novák <martinnovak@outlook.com>
2017-04-29 23:36:07 +0200

    next, prev - array

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,25 @@
 // [x] zjistit velikost rodičovského elementu a tu nastavit slideru
 // [x] při inicializaci vytvořit nový inner div, který bude mít nastavenou
 //     správnou šířku 
-// [ ] přidat tlačítka + promyslet, jak budou fungovat
+// [x] přidat tlačítka
+// [ ] vymyslet jak updatovat divy v innerDiv např po resizu, aby se správně nastavil offsetLeft, ...
+// [ ] vymyslet listener pro push v poli + updatovat stav slideru
 
 type SettingsType = {
     buttons?: Boolean;
+}
+
+type childrenOfDivType = {
+    element?: HTMLElement;
+    active?: boolean;
 }
 
 class Slider {
     elementName: string;
     elementHTML: HTMLElement;
     innerDiv: HTMLElement;
+    childrenOfDiv: HTMLElement[];
+    arrayOfChildren: childrenOfDivType[];
     dimensionOfParent: ClientRect;
     nextClick: number;
 
@@ -23,6 +32,19 @@ class Slider {
         // get name and element of slider
         this.elementName = element;
         this.elementHTML = $(element);
+        this.arrayOfChildren = [];
+
+        // get children and save it to array => NodeList to array
+        // only for the first time - later use method
+        let nodeList = $$(this.elementName + ' > *');
+        let childrenOfDiv = Array.prototype.slice.apply(nodeList);
+        childrenOfDiv.forEach((child, i) => {
+            this.arrayOfChildren.push({
+                element: child,
+                active: i === 0 ? true : false
+            });
+        });
+
         // get dimension of parent element - just in case
         this.dimensionOfParent = this.elementHTML.getBoundingClientRect();
 
@@ -35,30 +57,29 @@ class Slider {
     }
 
     sliderDimension() {
-
+        console.log(this.arrayOfChildren[0].element.offsetLeft);
     }
 
     init() {
         // document fragment
         const fragment = document.createDocumentFragment();
-        const innerDiv = document.createElement('div');
+        let innerDiv = document.createElement('div');
         innerDiv.classList.add('m-slider__inner');
 
-        // get children and save it to array => NodeList to array
-        const nodeList = $$(this.elementName + ' > *');
-        const childrenOfDiv = Array.prototype.slice.apply(nodeList);
-
         // width for new innerDiv = dimension * count of divs
-        const totalWidth = this.dimensionOfParent.width * childrenOfDiv.length;
+        let totalWidth = this.dimensionOfParent.width * this.arrayOfChildren.length;
         innerDiv.style.width = totalWidth + 'px';
         innerDiv.style.height = this.dimensionOfParent.height + 'px';
 
         // add classes to divs and parent div
         this.elementHTML.classList.add('m-slider__wrapper', 'm-slider__init');
-        childrenOfDiv.forEach(div => {
-            div.classList.add('m-slider__slide');
-            div.style.width = this.dimensionOfParent.width + 'px';
-            innerDiv.appendChild(div);
+        this.arrayOfChildren.forEach((child) => {
+            child.element.classList.add('m-slider__slide');
+            if (child.active) {
+                child.element.classList.add('m-slider__slide-active');
+            }
+            child.element.style.width = this.dimensionOfParent.width + 'px';
+            innerDiv.appendChild(child.element);
         });
 
         fragment.appendChild(innerDiv);
@@ -71,8 +92,8 @@ class Slider {
         const fragment = document.createDocumentFragment();
 
         if (settings.hasOwnProperty('buttons')) {
-            const nextButton = document.createElement('button');
-            const prevButton = document.createElement('button');
+            let nextButton = document.createElement('button');
+            let prevButton = document.createElement('button');
 
             nextButton.setAttribute('type',  'button');
             nextButton.setAttribute('title', 'Next slide');
@@ -80,8 +101,8 @@ class Slider {
             prevButton.setAttribute('type',  'button');
             prevButton.setAttribute('title', 'Previous slide');
 
-            nextButton.onclick = () => this.next(this.innerDiv);
-            prevButton.onclick = this.prev;
+            nextButton.onclick = () => this.next();
+            prevButton.onclick = () => this.prev();
 
             nextButton.textContent = 'Next';
             prevButton.textContent = 'Prev';
@@ -96,15 +117,40 @@ class Slider {
         this.elementHTML.appendChild(fragment);
     }
 
-    next(element?: HTMLElement): any {
-        this.nextClick += 1;
-        const totalTranslate = this.dimensionOfParent.width * this.nextClick;
-
-        element.style.transform = `translate3d(-${totalTranslate}px, 0, 0)`;
+    next() {
+        // get the active element
+        let indexOfActiveElement = this.arrayOfChildren.findIndex((child) => child.active);
+        // get count of elements
+        let lengthOfElements = this.arrayOfChildren.length;
+        // get current and next active element
+        let currentActive = this.arrayOfChildren[indexOfActiveElement];
+        let nextActive = this.arrayOfChildren[indexOfActiveElement + 1];
+        // change current and next active element's state
+        if (nextActive !== undefined) {
+            currentActive.active = !currentActive.active;
+            nextActive.active = !nextActive.active;
+        } else {
+            currentActive.active = !currentActive.active;
+            this.arrayOfChildren[0].active = true;
+        }
     }
 
     prev() {
-        console.log('prev');
+        // get the active element
+        let indexOfActiveElement = this.arrayOfChildren.findIndex((child) => child.active);
+        // get count of elements
+        let lengthOfElements = this.arrayOfChildren.length;
+        // get current and next active element
+        let currentActive = this.arrayOfChildren[indexOfActiveElement];
+        let prevActive = this.arrayOfChildren[indexOfActiveElement - 1];
+        // change current and next active element's state
+        if (prevActive !== undefined) {
+            currentActive.active = !currentActive.active;
+            prevActive.active = !prevActive.active;
+        } else {
+            currentActive.active = !currentActive.active;
+            this.arrayOfChildren[this.arrayOfChildren.length - 1].active = true;
+        }
     }
 }
 
